fb48d7e83cec020d94782f1a058711a5ee77477e
Matt Billenstein <matt@vazor.com>
2022-07-26 09:55:24 -0700

    Update config_local.sh

--- config_local.sh
+++ config_local.sh
@@ -1,19 +1,18 @@
 # install prefix
-VENV="/opt/ve"
+VENV="/opt/gf/ve"
 
 # cache package downloads
 PKG_CACHE="/tmp/venv-pkg"
 
 # build directory
 BUILD_DIR="$VENV/build"
-DATA_DIR="/opt/data"
-LOG_DIR="/opt/data/log"
-RUN_DIR="/opt/data/run"
+DATA_DIR="/opt/gf/data"
+LOG_DIR="/opt/gf/log"
+RUN_DIR="/opt/gf/run"
 
 # remote rsync for push/pull
-RSYNC_USER="me"
-RSYNC_HOST="me.com"
-RSYNC_PATH="ve"
+RSYNC_USER="push"
+RSYNC_HOST="mattb.gatherflora.dev"
 
 SUBPATH="$(echo $BUILDKITE_BRANCH | tr '/' '-')"
 if [ "$SUBPATH" == "" ]; then
