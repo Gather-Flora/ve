8cd7b40d7d360758aa4487e47407ba763f7b9528
Matt Billenstein <matt@vazor.com>
2022-09-16 17:52:34 +0000

    linux arm64

--- pkgs/available/nodejs.sh
+++ pkgs/available/nodejs.sh
@@ -1,8 +1,8 @@
 # mattb 2022/09/13 - use pre-built binaries, something on the macos x64 build
 # breaks npm currently down in npm minizlib...
 
-#NODEJS_VERSION="16.13.0"
-#NODEJS_SHA256SUM="9c00e5b6024cfcbc9105f9c58cf160762e78659a345d100c5bd80a7fb38c684f"
+#NODEJS_VERSION="16.17.0"
+#NODEJS_SHA256SUM="2a2e6262739741f98ab81648a50891861dbf66f12413b93f1a97b4c71570611e"
 #NODEJS_VERSION="14.18.3"
 #NODEJS_SHA256SUM="96d51324e4eb9dd88082a1effe328d272a6568121930e51ec089db1b966f891a"
 #NODEJS_VERSION="12.22.7"
@@ -22,7 +22,12 @@
 NODEJS_VERSION="16.17.0"
 NODEJS_PLATFORM="linux"
 NODEJS_ARCH="x64"
-NODEJS_SHA256SUM="f0867d7a17a4d0df7dbb7df9ac3f9126c2b58f75450647146749ef296b31b49b"
+NODEJS_SHA256SUM="4827808e50b8ee42b4dadf056835287dac267b9cff56cea56e70843bf8cecb79"
+
+if [ "$MARCH" == "arm64" ]; then
+  NODEJS_ARCH="arm64"
+  NODEJS_SHA256SUM="0e83e93bd3658f4ae516b5f1f174190bd87aaae3d691eb91a8945eed04dc8491"
+fi
 
 if [ "$MOS" == "MacOS" ]; then
   NODEJS_PLATFORM="darwin"
@@ -31,15 +36,11 @@ if [ "$MOS" == "MacOS" ]; then
     NODEJS_ARCH="arm64"
     NODEJS_SHA256SUM="96eefac1e168ec1bf39c5ae1e7b2760522624adfbe2e0c92875cd33ef9a07792"
   fi
-  NAME="node-v${NODEJS_VERSION}-${NODEJS_PLATFORM}-${NODEJS_ARCH}"
-  getpkg https://nodejs.org/dist/v${NODEJS_VERSION}/${NAME}.tar.gz $NODEJS_SHA256SUM
-  tar zxf ${NAME}.tar.gz
-else
-  NAME="node-v${NODEJS_VERSION}-${NODEJS_PLATFORM}-${NODEJS_ARCH}"
-  getpkg https://nodejs.org/dist/v${NODEJS_VERSION}/${NAME}.tar.xz $NODEJS_SHA256SUM
-  tar Jxf ${NAME}.tar.xz
 fi
 
-rm -f $NAME/[A-Z]*
+NAME="node-v${NODEJS_VERSION}-${NODEJS_PLATFORM}-${NODEJS_ARCH}"
+getpkg https://nodejs.org/dist/v${NODEJS_VERSION}/${NAME}.tar.gz $NODEJS_SHA256SUM
+tar zxf ${NAME}.tar.gz
 
+rm -f $NAME/[A-Z]*
 cp -r $NAME/* $VENV/
