198de6db7761618c080da015c3c44c449af23c2f
Matt Billenstein <matt@vazor.com>
2023-08-21 01:52:18 +0000

    Fix zlib url

--- pkgs/available/zlib.sh
+++ pkgs/available/zlib.sh
@@ -2,7 +2,7 @@ ZLIB_VERSION="1.2.13"
 ZLIB_SHA256SUM="b3a24de97a8fdbc835b9833169501030b8977031bcb54b3b3ac13740f846ab30"
 
 rm -fR zlib*
-getpkg https://zlib.net/zlib-${ZLIB_VERSION}.tar.gz $ZLIB_SHA256SUM
+getpkg https://www.zlib.net/fossils/zlib-${ZLIB_VERSION}.tar.gz $ZLIB_SHA256SUM
 tar zxf zlib-${ZLIB_VERSION}.tar.gz
 cd zlib-${ZLIB_VERSION}
 
