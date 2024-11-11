24f6c7038c2193eb5737c6bb524c80e49a170f73
Matt Billenstein <matt@vazor.com>
2024-11-11 11:31:20 -0800

    Newer libheif fixes problems - remove hack

--- pkgs/zz-site.sh
+++ pkgs/zz-site.sh
@@ -1,8 +1,1 @@
 # This is the last package built -- add your dependencies here
-
-# Fix pillow_heif to use our own heif lib - on linux they bundle all these
-# libs, not sure why not on macos...
-if [ "$MOS" == "MacOS" ]; then
-  install_name_tool -change @rpath/libheif.1.dylib $VENV/lib/libheif.1.dylib \
-    /opt/gf/ve/lib/python3.*/site-packages/_pillow_heif.cpython-3*-darwin.so
-fi
